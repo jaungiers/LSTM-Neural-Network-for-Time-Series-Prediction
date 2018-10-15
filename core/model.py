1a99987e0d965170d56b0a8ca75ffa489e4873bd
jaungiers <jakob@jakob-aungiers.com>
2018-10-15 21:50:20 +0100

    #39 fix generative training with > 1 epochs

--- core/model.py
+++ core/model.py
@@ -88,12 +88,14 @@ class Model():
 
 	def predict_point_by_point(self, data):
 		#Predict each timestep given the last sequence of true data, in effect only predicting 1 step ahead each time
+		print('[Model] Predicting Point-by-Point...')
 		predicted = self.model.predict(data)
 		predicted = np.reshape(predicted, (predicted.size,))
 		return predicted
 
 	def predict_sequences_multiple(self, data, window_size, prediction_len):
 		#Predict sequence of 50 steps before shifting prediction run forward by 50 steps
+		print('[Model] Predicting Sequences Multiple...')
 		prediction_seqs = []
 		for i in range(int(len(data)/prediction_len)):
 			curr_frame = data[i*prediction_len]
@@ -107,6 +109,7 @@ class Model():
 
 	def predict_sequence_full(self, data, window_size):
 		#Shift the window by 1 new prediction each time, re-run predictions on new window
+		print('[Model] Predicting Sequences Full...')
 		curr_frame = data[0]
 		predicted = []
 		for i in range(len(data)):
