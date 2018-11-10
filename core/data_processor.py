511d8711d0fbb8d5387eeee68bb3a491d882e225
fgadaleta <francesco.gadaleta@gmail.com>
2018-11-10 16:59:39 +0100

    Division by zero issue

--- core/data_processor.py
+++ core/data_processor.py
@@ -72,12 +72,13 @@ class DataLoader():
 
     def normalise_windows(self, window_data, single_window=False):
         '''Normalise window with a base value of zero'''
+        eps = 0.00001
         normalised_data = []
         window_data = [window_data] if single_window else window_data
         for window in window_data:
             normalised_window = []
             for col_i in range(window.shape[1]):
-                normalised_col = [((float(p) / float(window[0, col_i])) - 1) for p in window[:, col_i]]
+                normalised_col = [((float(p) / (float(window[0, col_i]) + eps) ) - 1) for p in window[:, col_i]]
                 normalised_window.append(normalised_col)
             normalised_window = np.array(normalised_window).T # reshape and transpose array back into original multidimensional format
             normalised_data.append(normalised_window)
